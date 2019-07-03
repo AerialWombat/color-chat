7c2f6b0b95b425ea0840b28164f295eebcd28d1d
AerialWombat <adrianzhan@gmail.com>
2019-07-03 18:07:05 -0400

    initial UI structure

--- public/index.js
+++ public/index.js
@@ -2,16 +2,52 @@ window.onload = () => {
 	$(document).ready(function() {
 		var socket = io();
 
-		$('form').submit(function(event) {
-			event.preventDefault();
+		const $sidebarButton = $('#sidebar-btn');
+		const $membersButton = $('#members-btn');
+		const $overlay = $('.dark-overlay');
+		const $sidebar = $('.sidebar');
+		const $members = $('.members');
 
-			socket.emit('chat message', $('.message-box').val());
-			$('.message-box').val('');
+		$sidebarButton.on('click', function() {
+			$overlay.fadeToggle();
+			$sidebar.addClass('show');
+		});
+
+		$membersButton.on('click', function() {
+			$overlay.fadeToggle();
+			$members.addClass('show');
+		});
+
+		$overlay.on('click', function() {
+			$overlay.fadeToggle();
+			$sidebar.removeClass('show');
+			$members.removeClass('show');
+		});
+
+		/* Socket
+======================================*/
+
+		$('.messages--form').on('submit', function() {
+			event.preventDefault();
+			const messageInput = $('.messages--form--input').val();
+
+			// Check for empty message
+			if (messageInput.length < 1) {
+				return false;
+			}
+
+			socket.emit('chat message', $('.messages--form--input').val());
+			$('.messages--form--input').val('');
 			return false;
 		});
 
 		socket.on('chat message', message => {
-			$('.messages').append($('<li>').text(message));
+			$('.messages--display').append(
+				$('<li>')
+					.text(message)
+					.hide()
+					.fadeIn(100)
+			);
 		});
 	});
 };
